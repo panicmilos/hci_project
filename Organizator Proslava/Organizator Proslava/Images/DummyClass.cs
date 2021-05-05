a30b20ace3ca16208eba3f50741c8799616cf784
Panic <panic.milos99@gmail.com>
2021-05-05 14:18:49 +0200

    feat: added folders for initial structure.

---
+++ Organizator Proslava/Organizator Proslava/Images/DummyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// This folder should only contains images.
+/// </summary>
+namespace Organizator_Proslava.Images
+{
+}
---
+++ Organizator Proslava/Organizator Proslava/Model/DummyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organizator_Proslava.Model
+{
+    /// <summary>
+    /// This class exists only so that this folder will be pushed on git.
+    /// Remove this class when this folder have at least one real class.
+    /// </summary>
+    internal class DummyClass
+    {
+    }
+}
