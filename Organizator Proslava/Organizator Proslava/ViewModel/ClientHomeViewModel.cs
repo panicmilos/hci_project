01ba9c7c7e26d23ab1181a9dd30bc500e7485bd0
Luka Bjelica <bjelicaluka99@gmail.com>
2021-05-18 18:48:52 +0200

    feat: implemented Client Home Page

---
+++ Organizator Proslava/Organizator Proslava/ViewModel/ClientHomeViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Input;
+using Organizator_Proslava.Dialogs.Option;
+using Organizator_Proslava.Dialogs.Service;
+using Organizator_Proslava.Model;
+using Organizator_Proslava.Utility;
+
+namespace Organizator_Proslava.ViewModel
+{
+    public class ClientHomeViewModel
+    {
+        public ICommand Cancel { get; set; }
+        public ICommand Back { get; set; }
+        
+        public IEnumerable<Celebration> Celebrations { get; set; } = new List<Celebration>
+        {
+            new Celebration
+            {
+                Type = "Rodjendan",
+                Organizer = new Organizer
+                {
+                    FirstName = "Milan",
+                    LastName = "Susic"
+                },
+                DateTime = DateTime.Now
+            }
+        };
+        
+        public ClientHomeViewModel()
+        {
+            Cancel = new RelayCommand(() =>
+                new DialogService().OpenDialog(new DialogWindow(),
+                    new OptionDialogViewModel("Potvrda otkazivanja proslave",
+                        "Da li ste sigurni da želite da otkažete proslavu?")));
+            
+            Back = new RelayCommand(() => EventBus.FireEvent("BackToLogin"));
+        }
+    }
+}
