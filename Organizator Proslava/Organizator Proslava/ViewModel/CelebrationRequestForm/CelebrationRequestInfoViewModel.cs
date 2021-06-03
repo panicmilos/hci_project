ac33deafe582b73a5e4b35e79a59d378d607ce86
Luka Bjelica <bjelicaluka99@gmail.com>
2021-06-03 23:33:48 +0200

    feat: searchable dropdown for celebration type in celebration req info form

--- Organizator Proslava/Organizator Proslava/ViewModel/CelebrationRequestForm/CelebrationRequestInfoViewModel.cs
+++ Organizator Proslava/Organizator Proslava/ViewModel/CelebrationRequestForm/CelebrationRequestInfoViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Organizator_Proslava.Dialogs.Map;
 using Organizator_Proslava.Dialogs.Service;
 using Organizator_Proslava.Model;
@@ -11,16 +12,25 @@ namespace Organizator_Proslava.ViewModel.CelebrationRequestForm
     {
         public Celebration Celebration { get; set; }
         
+        public ObservableCollection<string> CelebrationTypes { get; set; }
+        
         public ICommand OpenOrganizersDialog { get; set; }
         public ICommand OpenMap { get; set; }
         public ICommand Back { get; set; }
         public ICommand Next { get; set; }
 
+        private readonly ICelebrationTypeService _celebrationTypeService;
         private readonly IDialogService _dialogService;
 
-        public CelebrationRequestInfoViewModel(IOrganizerService organizerService, IDialogService dialogService)
+        public CelebrationRequestInfoViewModel(
+            ICelebrationTypeService celebrationTypeService,
+            IOrganizerService organizerService,
+            IDialogService dialogService)
         {
+            _celebrationTypeService = celebrationTypeService;
             _dialogService = dialogService;
+            
+            CelebrationTypes = new ObservableCollection<string>(_celebrationTypeService.ReadNames());
             
             OpenOrganizersDialog = new RelayCommand(() =>
             {
