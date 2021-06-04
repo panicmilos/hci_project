86b9a4c1d0e9a11f642f3e3ea7e007d022ab1697
Luka Bjelica <bjelicaluka99@gmail.com>
2021-06-04 13:44:27 +0200

    feat: implemented celebration proposals table

---
+++ Organizator Proslava/Organizator Proslava/ViewModel/CelebrationProposals/CelebrationProposalsViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using Organizator_Proslava.Data;
+using Organizator_Proslava.Model;
+using Organizator_Proslava.Model.CelebrationResponses;
+using Organizator_Proslava.Services.Contracts;
+using Organizator_Proslava.Utility;
+
+namespace Organizator_Proslava.ViewModel.CelebrationProposals
+{
+    public class CelebrationProposalsViewModel : NavigabileModelView
+    {
+        private Celebration _celebration;
+        public Celebration Celebration
+        {
+            get => _celebration;
+            set
+            {
+                _celebration = value;
+                _crtvm.Celebration = value;
+            }
+        }
+
+        private readonly ICelebrationResponseService _celebrationResponseService;
+        private readonly DatabaseContext _context;
+
+        private readonly CelebrationResponsesTableViewModel _crtvm;
+        private readonly CelebrationDetailsTableViewModel _cdtvm;
+        private readonly CelebrationProposalsTableViewModel _cptvm;
+        
+        public CelebrationProposalsViewModel(
+            CelebrationResponsesTableViewModel crtvm,
+            CelebrationDetailsTableViewModel cdtvm,
+            CelebrationProposalsTableViewModel cptvm,
+            ICelebrationResponseService celebrationResponseService,
+            DatabaseContext context)
+        {
+            Switch(crtvm);
+            
+            _celebrationResponseService = celebrationResponseService;
+            _context = context;
+
+            _crtvm = crtvm;
+            _cdtvm = cdtvm;
+            _cptvm = cptvm;
+            
+            EventBus.RegisterHandler("BackToCelebrationResponsesTable", () => Switch(_crtvm));
+            EventBus.RegisterHandler("BackToCelebrationDetailsTable", cr =>
+            {
+                _cdtvm.CelebrationResponse = cr as CelebrationResponse;
+                Switch(_cdtvm);
+            });
+            EventBus.RegisterHandler("SwitchCelebrationProposalsViewModel", vm => Switch(vm));
+        }
+
+    }
+}
