90cc7ca135bb2fa8e33ddb00b11a6916a0e47f5a
Panic <panic.milos99@gmail.com>
2021-06-05 18:27:17 +0200

    feat: integrated web help.

---
+++ Organizator Proslava/Organizator Proslava/Help/HelpViewer.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Organizator_Proslava.Help
+{
+    /// <summary>
+    /// Interaction logic for HelpViewer.xaml
+    /// </summary>
+    public partial class HelpViewer : Window
+    {
+        private readonly JavaScriptControlHelper ch;
+
+        public HelpViewer(string key, Window originator)
+        {
+            InitializeComponent();
+            string curDir = Directory.GetCurrentDirectory();
+            string path = String.Format("{0}/Documentation/{1}.htm", curDir, key);
+            if (!File.Exists(path))
+            {
+                key = "error";
+            }
+            Uri u = new Uri(String.Format("file:///{0}/Documentation/{1}.htm", curDir, key));
+            ch = new JavaScriptControlHelper(originator);
+            wbHelp.ObjectForScripting = ch;
+            wbHelp.Navigate(u);
+        }
+
+        private void BrowseBack_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ((wbHelp != null) && (wbHelp.CanGoBack));
+        }
+
+        private void BrowseBack_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbHelp.GoBack();
+        }
+
+        private void BrowseForward_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ((wbHelp != null) && (wbHelp.CanGoForward));
+        }
+
+        private void BrowseForward_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbHelp.GoForward();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+        }
+
+        private void wbHelp_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+        }
+    }
+}
