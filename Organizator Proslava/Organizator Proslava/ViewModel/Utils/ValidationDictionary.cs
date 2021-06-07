a204737c08c54cc38de3fb2fb98e1bfd7ea60a70
Panic <panic.milos99@gmail.com>
2021-06-07 12:16:59 +0200

    feat: implemented algorithms for JMBG, PIB, MB

--- Organizator Proslava/Organizator Proslava/ViewModel/Utils/ValidationDictionary.cs
+++ Organizator Proslava/Organizator Proslava/ViewModel/Utils/ValidationDictionary.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Organizator_Proslava.ViewModel.Utils
 {
@@ -117,18 +119,59 @@ namespace Organizator_Proslava.ViewModel.Utils
             return null;
         }
 
-        private static string ValidatePIB(object PIB, object _)
+        private static string ValidatePIB(object PIBObject, object _)
         {
-            if (string.IsNullOrWhiteSpace(PIB as string))
+            var PIB = PIBObject as string;
+            if (string.IsNullOrWhiteSpace(PIB))
                 return "Morate zadati PIB.";
+
+            if (PIB.Any(c => !Char.IsDigit(c)))
+                return "PIB morati imati samo cifre.";
+
+            if (PIB.Length != 9)
+                return "PIB nije ispravan.";
+
+            if (Double.Parse(PIB.Substring(0, 8)) < 10000001)
+                return "PIB nije ispravan.";
+
+            var rest = 10;
+            for (int i = 0; i < 8; i++)
+            {
+                rest = (PIB[i] - '0' + rest) % 10;
+                if (rest == 0)
+                    rest = 10;
+
+                rest = rest * 2 % 11;
+            }
+            if ((PIB[8] - '0') != ((11 - rest) % 10))
+                return "PIB nije ispravan.";
 
             return null;
         }
 
-        private static string ValidateIdentificationNumber(object identificationNumber, object _)
+        private static string ValidateIdentificationNumber(object MBObject, object _)
         {
-            if (string.IsNullOrWhiteSpace(identificationNumber as string))
+            var MB = MBObject as string;
+            if (string.IsNullOrWhiteSpace(MB as string))
                 return "Morate zadati matični broj.";
+
+            if (MB.Any(c => !Char.IsDigit(c)))
+                return "Matični broj morati imati samo cifre.";
+
+            if (MB.Length != 8)
+                return "Matični broj nije ispravan.";
+
+            var L = 0;
+            for (int i = MB.Length - 2, mnozilac = 2; i >= 0; i--)
+            {
+                L += (MB[i] - '0') * mnozilac;
+                mnozilac = mnozilac == 7 ? 2 : mnozilac + 1;
+            }
+            L = 11 - (L % 11);
+            L = L > 9 ? 0 : L;
+
+            if (MB[7] - '0' != L)
+                return "Matični broj nije ispravan.";
 
             return null;
         }
@@ -141,18 +184,39 @@ namespace Organizator_Proslava.ViewModel.Utils
             return null;
         }
 
-        private static string ValidateJMBG(object JMBG, object _)
+        private static string ValidateJMBG(object JMBGObject, object _)
         {
+            var JMBG = JMBGObject as string;
             if (string.IsNullOrWhiteSpace(JMBG as string))
                 return "Morate zadati JMBG.";
+
+            if (JMBG.Any(c => !Char.IsDigit(c)))
+                return "JMBG morati imati samo cifre.";
+
+            if (JMBG.Length != 13)
+                return "JMBG nije ispravan.";
+
+            var L = 11 - ((7 * (JMBG[0] - '0' + JMBG[6] - '0') + 6 * (JMBG[1] - '0' + JMBG[7] - '0') + 5 * (JMBG[2] - '0' + JMBG[8] - '0') +
+                4 * (JMBG[3] - '0' + JMBG[9] - '0') + 3 * (JMBG[4] - '0' + JMBG[10] - '0') + 2 * (JMBG[5] - '0' + JMBG[11] - '0')) % 11);
+            L = L <= 9 ? L : 0;
+
+            if (JMBG[12] - '0' != L)
+                return "JMBG nije ispravan.";
 
             return null;
         }
 
-        private static string ValidatePersonalId(object personalId, object _)
+        private static string ValidatePersonalId(object personalIdObject, object _)
         {
+            var personalId = personalIdObject as string;
             if (string.IsNullOrWhiteSpace(personalId as string))
-                return "Morate zadati matični broj.";
+                return "Morate zadati broj lične karte.";
+
+            if (personalId.Any(c => !Char.IsDigit(c)))
+                return "Broj lične karte morati imati samo cifre.";
+
+            if (personalId.Length != 9)
+                return "Broj lične karte nije ispravan.";
 
             return null;
         }
