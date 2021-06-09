d040f9ef8e5734e6886f6f6184ab9c7d53868c11
Luka Bjelica <bjelicaluka99@gmail.com>
2021-06-09 19:23:04 +0200

    feat: colab service price added converter

---
+++ Organizator Proslava/Organizator Proslava/Converters/PriceToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Organizator_Proslava.Converters
+{
+    public class PriceToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is float price)
+            {
+                return price + " RSD";
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
