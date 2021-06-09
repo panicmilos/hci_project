d5b7a6a39834c2268781e7e1159ff8862821ca15
Luka Bjelica <bjelicaluka99@gmail.com>
2021-06-09 18:19:38 +0200

    refactor: datetime format in tables

---
+++ Organizator Proslava/Organizator Proslava/Converters/DateToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Organizator_Proslava.Converters
+{
+    public class DateToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime.ToString("dd/MM/yyyy h:mm");
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
