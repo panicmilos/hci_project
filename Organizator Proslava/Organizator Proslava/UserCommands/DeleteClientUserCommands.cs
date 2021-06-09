59cc38f9f60775158f46b8990da3e59502c4fb6a
Panic <panic.milos99@gmail.com>
2021-06-09 17:54:20 +0200

    feat: implemented command to undo/redo deleting client.

---
+++ Organizator Proslava/Organizator Proslava/UserCommands/DeleteClientUserCommands.cs
@@ -0,0 +1,34 @@
+﻿using Organizator_Proslava.Model;
+using Organizator_Proslava.Ninject;
+using Organizator_Proslava.Services.Contracts;
+using System.Collections.ObjectModel;
+
+namespace Organizator_Proslava.UserCommands
+{
+    public class DeleteClientUserCommands : IUserCommand
+    {
+        private readonly Client _client;
+        private readonly ObservableCollection<Client> _clients;
+
+        private readonly IClientService _clientService;
+
+        public DeleteClientUserCommands(Client client, ObservableCollection<Client> Clients)
+        {
+            _client = client;
+            _clients = Clients;
+            _clientService = ServiceLocator.Get<IClientService>();
+        }
+
+        public void Redo()
+        {
+            _clientService.Delete(_client.Id);
+            _clients.Remove(_client);
+        }
+
+        public void Undo()
+        {
+            _clientService.Create(_client);
+            _clients.Add(_client);
+        }
+    }
+}
